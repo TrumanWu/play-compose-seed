0df40d97596bdda2ae1a68e17bc8867f77e757b6
trumanwu <chunan.woo@gmail.com>
2015-04-05 19:32:56 +0800

    fix bug of sql evolution file.

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -2,7 +2,7 @@
 SET NAMES utf8;
 CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL COMMENT 'user name'
+  `name` varchar(255) NOT NULL COMMENT 'user name',
   `update_time` int(10) NOT NULL COMMENT 'last update timestamp',
   `init_time` int(10) NOT NULL COMMENT 'init timestamp',
   `tombstone` tinyint(1) unsigned NOT NULL DEFAULT '0',
